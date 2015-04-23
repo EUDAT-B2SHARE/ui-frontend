c69089e22fc085befcd2bef3861d4639ca4a34e2
Dennis Blommesteijn <dennis@blommesteijn.com>
2015-04-23 18:06:08 +0200

    Add missing files

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -40,8 +40,10 @@ var phonecatControllers = angular.module('phonecatControllers', ['angular-loadin
 phonecatControllers.controller('HomeCtrl', ['$scope', '$http', '$location', function($scope, $http, $location){
   $scope.searchForm = {};
   $scope.searchForm.submitForm = function() {
-    if($scope.searchForm.query == undefined)
+    if($scope.searchForm.query == undefined){
+      $scope.searchForm_errorMessage = "Please provide a search value";
       return;
+    }
     $scope.errorName = "blaat";
     $scope.message = "generic error";
     $location.path('/search/query/' + $scope.searchForm.query);
